3bce9e8f9a5f9334b8ec3acb5a30ac4a501b0b0d
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-07-21 20:24:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# NoiseContrastiveEstimation
+
+![CI](https://github.com/francescoalemanno/NoiseContrastiveEstimation.jl/workflows/CI/badge.svg?branch=master)
+[![Coverage](http://codecov.io/github/francescoalemanno/NoiseContrastiveEstimation.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/francescoalemanno/NoiseContrastiveEstimation.jl)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://francescoalemanno.github.io/NoiseContrastiveEstimation.jl/dev)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://francescoalemanno.github.io/NoiseContrastiveEstimation.jl/stable/)
---
+++ docs/make.jl
@@ -0,0 +1,22 @@
+using NoiseContrastiveEstimation
+using Documenter
+ENV["GKSwstype"] = "100"
+@show joinpath(@__DIR__, "src")
+
+makedocs(;
+    modules = [NoiseContrastiveEstimation],
+    authors = "Francesco Alemanno <francescoalemanno710@gmail.com> and contributors",
+    repo = "https://github.com/francescoalemanno/NoiseContrastiveEstimation.jl/blob/{commit}{path}#L{line}",
+    sitename = "NoiseContrastiveEstimation.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://francescoalemanno.github.io/NoiseContrastiveEstimation.jl",
+        assets = String[],
+    ),
+    pages = [
+        "Basic Usage" => "index.md",
+        "Reference" => "reference.md",
+    ],
+)
+
+deploydocs(; repo = "github.com/francescoalemanno/NoiseContrastiveEstimation.jl", push_preview = true)
