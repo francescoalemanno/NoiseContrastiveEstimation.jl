04885c55bdd82d0f5b639bab90348a2300d37f9c
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-07-22 10:16:15 +0200

    type stable

--- src/NoiseContrastiveEstimation.jl
+++ src/NoiseContrastiveEstimation.jl
@@ -9,8 +9,15 @@ end
 function (J::CNCE)(θ)
     κ=size(J.noised,1)
     N=length(J.data)
-    jn=0.0
-    for i=1:N
+
+    bϕ=J.lϕ(J.data[1],θ)
+    G=bϕ-J.lϕ(J.noised[1,1],θ)
+    jn=2/(κ*N)*log(1+exp(-G))
+    for j=2:κ
+        G=bϕ-J.lϕ(J.noised[j,1],θ)
+        jn+=2/(κ*N)*log(1+exp(-G))
+    end
+    for i=2:N
         bϕ=J.lϕ(J.data[i],θ)
         for j=1:κ
             G=bϕ-J.lϕ(J.noised[j,i],θ)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,5 @@ using Test
     data=randn(200).*0.3 .+ 1.5
     noised=foldl(hcat, x.+randn(20).*0.4  for x in data)
     J=CNCE(lϕ,data,noised)
-    @test J((15,-10)) < J((0,0))
+    @test J((15,-10)) < J((5,-5)) < J((0,0))
 end
