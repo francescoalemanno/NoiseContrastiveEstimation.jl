6a89e2addd0758e66477a27e5f2553215be68ac7
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-07-22 09:30:59 +0200

    added Functional

--- src/NoiseContrastiveEstimation.jl
+++ src/NoiseContrastiveEstimation.jl
@@ -1,3 +1,24 @@
 module NoiseContrastiveEstimation
 
+struct CNCE{T,F}
+    lϕ::F
+    data::Vector{T}
+    noised::Matrix{T}
+end
+
+function (J::CNCE)(θ)
+    κ=size(J.noised,1)
+    N=length(J.data)
+    jn=0.0
+    for i=1:N
+        bϕ=J.lϕ(J.data[i],θ)
+        for j=1:κ
+            G=bϕ-J.lϕ(J.noised[j,i],θ)
+            jn+=2/(κ*N)*log(1+exp(-G))
+        end
+    end
+    jn
+end
+
+export CNCE
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,9 @@
 using NoiseContrastiveEstimation
+using Test
+@testset "Basic Functional" begin
+    lϕ(x,θ) = θ[1]*x + θ[2]*x*x/2
+    data=randn(200).*0.3 .+ 1.5
+    noised=foldl(hcat, x.+randn(20).*0.4  for x in data)
+    J=CNCE(lϕ,data,noised)
+    @test J((15,-10)) < J((0,0))
+end
