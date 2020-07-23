95e61ac1ad2ce254ee68edb9c89276309134edb3
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-07-23 16:44:53 +0200

    added gradient of CNCE

--- src/NoiseContrastiveEstimation.jl
+++ src/NoiseContrastiveEstimation.jl
@@ -1,31 +1,46 @@
 module NoiseContrastiveEstimation
 
-struct CNCE{T,F}
+struct CNCE{T,F,dF}
     lϕ::F
+    gϕ::dF
     data::Vector{T}
     noised::Matrix{T}
 end
 
-function (J::CNCE)(θ)
-    κ=size(J.noised,1)
-    N=length(J.data)
+CNCE(lϕ, data, noised) = CNCE(lϕ, x -> error("Gradient is not available"), data, noised)
 
-    bϕ=J.lϕ(J.data[1],θ)
-    G=bϕ-J.lϕ(J.noised[1,1],θ)
-    jn=2/(κ*N)*log(1+exp(-G))
-    for j=2:κ
-        G=bϕ-J.lϕ(J.noised[j,1],θ)
-        jn+=2/(κ*N)*log(1+exp(-G))
-    end
-    for i=2:N
-        bϕ=J.lϕ(J.data[i],θ)
-        for j=1:κ
-            G=bϕ-J.lϕ(J.noised[j,i],θ)
-            jn+=2/(κ*N)*log(1+exp(-G))
-        end
-    end
-    jn
+struct Grad end
+struct Cost end
+
+function (J::CNCE)(::Cost, θ)
+    κ = size(J.noised, 1)
+    N = length(J.data)
+    foldl(+, begin
+        bϕ = J.lϕ(J.data[i], θ)
+        foldl(+, begin
+            G = bϕ - J.lϕ(J.noised[j, i], θ)
+            2 / (κ * N) * log(1 + exp(-G))
+        end for j = 1:κ)
+    end for i = 2:N)
 end
 
-export CNCE
+function (J::CNCE)(::Grad, θ)
+    κ = size(J.noised, 1)
+    N = length(J.data)
+
+    foldl(
+        +,
+        begin
+            bϕ = J.lϕ(J.data[i], θ)
+            dbϕ = J.gϕ(J.data[i], θ)
+            foldl(+, begin
+                G = bϕ - J.lϕ(J.noised[j, i], θ)
+                dG = dbϕ - J.gϕ(J.noised[j, i], θ)
+                2 / (κ * N) * (1 / (1 + exp(-G)) - 1) * dG
+            end for j = 1:κ)
+        end for i = 1:N
+    )
+end
+
+export CNCE, Grad, Cost
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,18 @@
 using NoiseContrastiveEstimation
 using Test
 @testset "Basic Functional" begin
-    lϕ(x,θ) = θ[1]*x + θ[2]*x*x/2
-    data=randn(200).*0.3 .+ 1.5
-    noised=foldl(hcat, x.+randn(20).*0.4  for x in data)
-    J=CNCE(lϕ,data,noised)
-    @test J((15,-10)) < J((5,-5)) < J((0,0))
+    lϕ(x, θ) = θ[1] * x + θ[2] * x * x / 2
+    gϕ(x, ϕ) = [x, x * x / 2]
+    data = randn(200) .* 0.3 .+ 1.5
+    noised = foldl(hcat, x .+ randn(20) .* 0.4 for x in data)
+    J = CNCE(lϕ, gϕ, data, noised)
+    @test J(Cost(), (15, -10)) < J(Cost(), (5, -5)) < J(Cost(), (0, 0))
+    x = zeros(2)
+    v = zeros(2)
+    while true
+        v = 0.9 .* v .- J(Grad(), x)
+        x = x .+ v
+        sum(abs2, v) < 1e-8 && break
+    end
+    @test J(Cost(), x) < J(Cost(), (15, -10))
 end
